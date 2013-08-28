61851c50384516e0a1fdf39cf92b8c0a0a7b11ac
benSlaughter <B.P.Slaughter@gmail.com>
2013-08-28 13:56:14 +0100

    Removed method files, moved methods into main allotment module and updated rspec

--- lib/allotment.rb
+++ lib/allotment.rb
@@ -1,6 +1,5 @@
 require 'allotment/array'
 require 'allotment/stopwatch'
-require 'allotment/methods'
 require 'json'
 
 module Allotment
@@ -36,4 +35,24 @@ module Allotment
 			JSON.pretty_generate @results
 		end
 	end
+
+	def record_event name = 'unnamed', &block
+    Allotment.record_event name, &block
+  end
+
+  def start_recording name = 'unnamed'
+    Allotment.start_recording name
+  end
+
+  def stop_recording name
+    Allotment.stop_recording name
+  end
+
+  def results
+    Allotment.results
+  end
+
+  def results_string
+    Allotment.results_string
+  end
 end
